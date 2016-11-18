c6beb8b9c40cfe6a569299c966484bdad71667fa
ENRIQUESOSA <kiqueyo847@gmail.com>
2016-11-18 00:08:07 -0600

    inicio

---
+++ Kozmoz/BussinesLayer/Prueba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+namespace BussinesLayer
+{
+    public class Prueba
+    {
+        public void store(){
+
+        }
+
+        public List<empresaservicio> index()
+        {
+            try
+            {
+                using (kosmozbusEntities dbContext = new kosmozbusEntities())
+                {
+                    var query = (from n in dbContext.empresaservicios
+                                 select n).ToList();
+                    return query;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
