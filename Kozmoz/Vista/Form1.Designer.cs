66c14cedbf438a31b780e3d5bdc3401268115d70
ENRIQUESOSA <kiqueyo847@gmail.com>
2016-11-18 00:17:23 -0600

    vista1

---
+++ Kozmoz/Vista/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Vista
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.datos = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.datos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // datos
+            // 
+            this.datos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datos.Location = new System.Drawing.Point(91, 151);
+            this.datos.Name = "datos";
+            this.datos.Size = new System.Drawing.Size(240, 150);
+            this.datos.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(195, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 318);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.datos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.datos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView datos;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Kozmoz/Vista/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BussinesLayer;
+using DataModel;
+namespace Vista
+{
+    public partial class Form1 : Form
+    {
+        Prueba pr = new Prueba();
+        public Form1()
+        {
+            InitializeComponent();
+            datos.DataSource = pr.index();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            datos.DataSource = pr.index();
+        }
+    }
+}
